f971dfdcaceaca939cc357254c1fd2816a428001
aakash-sriv <aakashsriv06@gmail.com>
2025-09-02 02:27:19 +0530

    temp

--- flowmind/components/header.jsx
+++ flowmind/components/header.jsx
@@ -2,6 +2,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
  
 
 const Header = () => {
@@ -15,16 +16,38 @@ const Header = () => {
         width={200}
         className="h-12 w-auto object-contain"
         />
-      </Link>
-      
-      <div>
+      </Link>      
+
+      <div className="flex items-center space-x-4">
+        <SignedIn>
+          <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+            <Button variant="outline">
+              <LayoutDashboard size={18} />
+              <span className="hidden md:inline">Dashboard</span>
+            </Button>
+          </Link>
+
+
+          <Link href={"/transaction/create"}>
+            <Button className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span className="hidden md:inline">Add Transaction</span>
+            </Button>
+          </Link>
+        </SignedIn>
+
+
         <SignedOut>
             <SignInButton forceRedirectUrl="/dashboard">
               <Button variant="outline">Login</Button>
             </SignInButton>
         </SignedOut>
         <SignedIn>
-            <UserButton />
+            <UserButton appearance={{
+              elements: {
+                avatarBox: "w-20 h-20",
+              },
+            }} />
         </SignedIn>
       </div>
     </nav>
