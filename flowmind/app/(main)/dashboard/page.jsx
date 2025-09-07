37da8514fb9973be0800e6bef914c49872b551ee
aakash-sriv <aakashsriv06@gmail.com>
2025-09-07 23:27:42 +0530

    temp

---
+++ flowmind/app/(main)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+  <div className='px-5'>
+    <h1 className='text-6xl font-bold gradient-title mb-5'>Dashboard</h1>
+    {/* dashboard page */}
+  </div>
+  )
+}
+
+export default Dashboard
