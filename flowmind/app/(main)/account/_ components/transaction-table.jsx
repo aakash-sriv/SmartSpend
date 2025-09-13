4b211c068d565a88a67bc679b374686cb7f9c2e5
aakash-sriv <aakashsriv06@gmail.com>
2025-09-13 16:27:46 +0530

    commit1

---
+++ flowmind/app/(main)/account/_ components/transaction-table.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Checkbox } from '@/components/ui/checkbox'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import React from 'react'
+
+const Transactiontable = ({transactions}) => {
+
+    const handleSort = () => {
+
+    };
+
+
+
+
+
+
+  return (
+    <div className='space-y-4'>
+        {/* Filter */}
+
+        {/* Transactions table */}
+        <div className='rouned-md border'>            
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[50px]">
+                            <Checkbox />
+                        </TableHead>
+                        <TableHead 
+                            className="cursor-pointer"
+                            onClick={() => handleSort("date")}
+                        >
+                            <div className='flex items-center'>Date</div>
+                        </TableHead>
+                        <TableHead>Description</TableHead>
+                        <TableHead 
+                            className="cursor-pointer"
+                            onClick={() => handleSort("category")}
+                        >
+                            <div className='flex items-center'>Category</div>
+                        </TableHead>
+                        <TableHead 
+                            className="cursor-pointer"
+                            onClick={() => handleSort("amount")}
+                        >
+                            <div className='flex items-center justify-end'>Amount</div>
+                        </TableHead>
+                        <TableHead>Recurring</TableHead>
+                        <TableHead className={`w-[50px]`} />
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">₹250.00</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    </div>
+  )
+}
+
+export default Transactiontable
