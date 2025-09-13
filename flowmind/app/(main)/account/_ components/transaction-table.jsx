ccb78afdbe1ef815b520ef3b016d38ca7d849cb0
aakash-sriv <aakashsriv06@gmail.com>
2025-09-13 23:52:02 +0530

    commit4

--- flowmind/app/(main)/account/_ components/transaction-table.jsx
+++ flowmind/app/(main)/account/_ components/transaction-table.jsx
@@ -1,11 +1,21 @@
 "use client";
 
+import { Badge } from '@/components/ui/badge';
 import { Checkbox } from '@/components/ui/checkbox'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { categoryColors } from '@/data/categories';
 import { format } from 'date-fns/format';
+import { Clock, RefreshCw } from 'lucide-react';
 import React from 'react'
 import { fi } from 'zod/v4/locales';
+
+const RECURRING_INTERVALS = { 
+    DAILY: "Daily",
+    WEEKLY: "Weekly",
+    MONTHLY: "Monthly",
+    YEARLY: "Yearly",
+}
 
 const Transactiontable = ({transactions}) => {
 
@@ -80,7 +90,47 @@ const Transactiontable = ({transactions}) => {
                                     {transaction.category}
                                 </span>                               
                             </TableCell>
-                            <TableCell className="text-right">₹250.00</TableCell>
+                            <TableCell className="text-right font-medium"
+                                style={{
+                                    color: transaction.type === "EXPENSE" ? "red" : "green",
+                                }}
+                            >
+                                {transaction.type === "EXPENSE" ? "-" : "+"} ₹  
+                                {transaction.amount.toFixed(2)}
+                            </TableCell>
+                            <TableCell>
+                                {transaction.isRecurring ? (
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <Badge variant="outline" className={`gap-1 bg-purple-100 hover:bg-purple-200 text-fuchsia-600`}>
+                                                    <RefreshCw className='h-3 w-3'/>
+                                                        {
+                                                        RECURRING_INTERVALS[
+                                                            transaction.recurringInterval
+                                                            ]
+                                                        }
+                                                </Badge>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <div className='text-sm'>
+                                                    <div className='font-medium'>Next Date:</div>
+                                                    <div>
+                                                        {format(
+                                                            new Date(transaction.nextRecurringDate),"pp"
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                ):(
+                                    <Badge variant="outline" className={`gap-1`}>
+                                        <Clock className='h-3 w-3'/>
+                                        One-time
+                                    </Badge>
+                                )}
+                            </TableCell>
                         </TableRow>
                         ))
                     )}
