5c5501300c10648be62655bb02de770cc7ae6b08
aakash-sriv <aakashsriv06@gmail.com>
2025-09-23 23:54:24 +0530

    -

--- smartspend/app/(main)/transaction/_components/transaction-form.jsx
+++ smartspend/app/(main)/transaction/_components/transaction-form.jsx
@@ -167,6 +167,14 @@ const AddTransactionForm = ({accounts , categories}) => {
         )}
       </div>
 
+      <div className='space-y-2'>
+        <label className='text-sm font-medium'>Description</label>
+        <Input placeholder= "Enter description" {...register("description")} />
+        {errors.description && (
+          <p className='text-sm text-red-500'>{errors.description.message}</p>
+        )}
+      </div>
+
     </form>
   )
 }
