be627ac8d91c848213395b3f3e016011991f2474
aakash-sriv <aakashsriv06@gmail.com>
2025-09-28 20:01:31 +0530

    overview done

--- smartspend/app/api/inngest/route.js
+++ smartspend/app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert, processRecurringTransactions, triggerRecurringTransactions } from "@/lib/inngest/functions";
+import { checkBudgetAlert, generateMonthlyReports, processRecurringTransactions, triggerRecurringTransactions } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
@@ -8,6 +8,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     checkBudgetAlert , 
     triggerRecurringTransactions , 
-    processRecurringTransactions
+    processRecurringTransactions,
+    generateMonthlyReports
   ],
 });
--- smartspend/lib/inngest/functions.js
+++ smartspend/lib/inngest/functions.js
@@ -4,6 +4,8 @@ import { inngest } from "./client";
 import EmailTemplate from "@/emails/template";
 import { transactionSchema } from "@/app/lib/schema";
 import { tr } from "zod/v4/locales";
+import { gte } from "zod";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export const checkBudgetAlert = inngest.createFunction(
   { name: "Check Budget-Alerts" },
@@ -45,7 +47,7 @@ export const checkBudgetAlert = inngest.createFunction(
                     where: {
                         userId: budget.userId,
                         accountId: defaultAccount.id,
-                        type: 'EXPENSE',
+                        type: "EXPENSE",
                         date: {
                             gte: startOfMonth,
                             lte: endOfMonth,
@@ -79,9 +81,7 @@ export const checkBudgetAlert = inngest.createFunction(
                                     accountName: defaultAccount.name,
                                 } 
                             })
-                        })
-
-
+                        });
 
 
                         //Update lastAlertSent
@@ -240,4 +240,128 @@ function calculateNextRecurringDate(startDate, interval) {
   }
 
   return date;
+}
+
+export const generateMonthlyReports = inngest.createFunction(
+    {
+        id: "generate-monthly-reports",
+        name: " Generate Monthly Reports",
+    },
+    { cron: "0 0 1 * *" },
+    async({step}) => {
+        const users = await step.run("fetch-users" ,async () => {
+            return await db.user.findMany({
+                include: { accounts: true },
+            });
+        });
+
+        for(const user of users) {
+            await step.run(`generate-report-${user.id}`, async() => {
+                const lastMonth = new Date();
+                lastMonth.setMonth(lastMonth.getMonth() -1);
+
+                const stats = await getMonthlyStats(user.id , lastMonth);
+                const monthName = lastMonth.toLocaleString("default" , {
+                    month: "long",
+                });
+
+            const insights = await generateFinancialInsights(stats ,monthName);
+
+            //Send Email
+                await sendEmail({
+                    //to : user.email
+                    to:  "aakashsriv06@gmail.com",
+                    subject: `Your Monthly Financial Report - ${monthName}`,
+                    react : EmailTemplate({
+                        userName: user.name ,
+                        type: "monthly-report",  
+                        data: {
+                            stats,
+                            month: monthName,
+                            insights,
+                        }, 
+                    }),
+                });
+            });
+        }
+
+        return { processed: users.length }
+    }
+);
+
+async function generateFinancialInsights(stats ,monthName ){
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({ model : "gemini-2.0-flash" });
+
+
+    const prompt = `
+    Analyze this financial data and provide 3 concise, actionable insights.
+    Focus on spending patterns and practical advice.
+    Keep it friendly and conversational.
+
+    Financial Data for ${monthName}:
+    - Total Income: ₹${stats.totalIncome}
+    - Total Expenses: ₹${stats.totalExpenses}
+    - Net Income: ₹${(stats.totalIncome - stats.totalExpenses)}
+    - Expense Categories: ${Object.entries(stats.byCategory)
+      .map(([category, amount]) => `${category}: ₹${amount}`)
+      .join(", ")}
+
+    Format the response as a JSON array of strings, like this:
+    ["insight 1", "insight 2", "insight 3"]
+  `;
+
+  try {
+    const result = await model.generateContent(prompt);
+
+    const response = await result.response;
+    const text = response.text();
+    const cleanedText = text.replace(/```(?:json)?\n?/g, "").trim();
+
+    return JSON.parse(cleanedText);
+    
+  } catch (error) {
+    console.error("Error generating insights:" , error);
+    return [
+        "Your highest expense category this monthmight need attention.",
+        "Consider setting up a budget for better financial management.",
+        "Track your recurring expenses to identify potential savings."
+    ];
+  }
+}
+
+
+const getMonthlyStats = async (userId ,month ) => {
+    const startDate = new Date(month.getFullYear() , month.getMonth(), 1);
+    const endDate = new Date(month.getFullYear() , month.getMonth()+ 1 ,0);
+
+    const transactions = await db.transaction.findMany({
+        where: {
+            userId,
+            date: {
+                gte: startDate,
+                lte: endDate,
+            }
+        }
+    });
+
+    return transactions.reduce(
+        (stats, t ) => {
+            const amount = t.amount.toNumber();
+            if(t.type === "EXPENSE") {
+                stats.totalExpenses += amount;
+                stats.byCategory[t.category] = 
+                    (stats.byCategory[t.category] || 0) + amount;
+            } else {
+                stats.totalIncome += amount;
+            }
+            return stats;
+        },
+        {
+            totalExpenses : 0,
+            totalIncome : 0,
+            byCategory : {},
+            transactionCount : transactions.length,
+        }
+    )
 }
